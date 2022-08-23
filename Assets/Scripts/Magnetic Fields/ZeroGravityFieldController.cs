5d8adbc80a7eb52598035a88620a6040943f9041
nikitasakkas <nikitasakkas@gmail.com>
2022-08-23 23:54:29 +0300

    made gravity removal and reapplication more efficient

--- Assets/Scripts/Magnetic Fields/ZeroGravityFieldController.cs
+++ Assets/Scripts/Magnetic Fields/ZeroGravityFieldController.cs
@@ -7,6 +7,7 @@ public class ZeroGravityFieldController : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collider)
     {
         disableVerticalControl(collider.gameObject);
+        applyPlayerGravityPull(collider.attachedRigidbody);
     }
 
     private void OnTriggerStay2D(Collider2D collider)
@@ -14,20 +15,26 @@ public class ZeroGravityFieldController : MonoBehaviour
         enableVerticalControl(collider.gameObject);
         if (collider.GetComponent<Collider2D>().tag == "Player")
         {
-            Rigidbody2D playerRB = collider.attachedRigidbody;
-            removePlayerGravityPull(playerRB);
+            removePlayerGravityPull(collider.attachedRigidbody);
         }
     }
-
+    
     private void removePlayerGravityPull(Rigidbody2D playerRB)
     {
         playerRB.velocity = Vector2.zero;
-        playerRB.AddForce(-Physics.gravity * playerRB.mass);
+        playerRB.GetComponent<Rigidbody2D>().gravityScale = 0; // remove gravity pull
     }
+    private void applyPlayerGravityPull(Rigidbody2D playerRB)
+    {
+        playerRB.velocity = Vector2.zero;
+        playerRB.GetComponent<Rigidbody2D>().gravityScale = 1; // apply gravity pull
+    }
+
     private void enableVerticalControl(GameObject player)
     {
         player.GetComponent<CharacterController2D>().verticalControl = true;
     }
+
     private void disableVerticalControl(GameObject player)
     {
         player.GetComponent<CharacterController2D>().verticalControl = false;
