aa691f5788defdb3ed9234bf6b2f89987eaed34b
nikitasakkas <nikitasakkas@gmail.com>
2022-10-21 18:09:08 +0300

    bullets/explosions destroy enemies

---
+++ Assets/Scripts/Particle Explosions/EnemyDamager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamager : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(removeCollider());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.SetActive(false);
+    }
+
+
+    IEnumerator removeCollider()
+    {
+        yield return new WaitForSeconds(0.4f);
+        gameObject.GetComponent<Collider2D>().enabled = false;
+    }
+}
