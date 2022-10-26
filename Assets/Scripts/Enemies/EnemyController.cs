98e5ddfbd3e5e27026228d7b9bdc8485d0aadd18
nikitasakkas <nikitasakkas@gmail.com>
2022-10-26 23:07:23 +0300

    max chase distance added to enemy

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -6,11 +6,29 @@ using Pathfinding;
 public class EnemyController : MonoBehaviour
 {
     public AIPath aiPath;
+    public GameObject player;
+    public float minDistance;
 
-    // Update is called once per frame
+    private float distance;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
     void Update()
     {
-        if(aiPath.desiredVelocity.x >= 0.01f)
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Debug.Log(distance);
+        if(distance > minDistance)
+        {
+            gameObject.GetComponent<AIDestinationSetter>().enabled = false;
+        }
+        else
+        {
+            gameObject.GetComponent<AIDestinationSetter>().enabled = true;
+        }
+        
+            if(aiPath.desiredVelocity.x >= 0.01f)
         {
             transform.localScale = new Vector3(-1f, 1f, 1f);
         }
