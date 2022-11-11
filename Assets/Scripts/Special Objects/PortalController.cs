e390ace843afc4e988557c459872caf25e5a1c5a
nikitasakkas <nikitasakkas@gmail.com>
2022-11-11 23:59:32 +0200

    added portal color!

--- Assets/Scripts/Special Objects/PortalController.cs
+++ Assets/Scripts/Special Objects/PortalController.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
 
 public class PortalController : MonoBehaviour
 {
     public bool isActive;
     GameController gameController;
     SpriteRenderer m_SpriteRenderer;
+    public Color inactiveLightColor = new Color(0.61f, 0.16f, 1f, 1f);
+    public Color activeLightColor = new Color(1, 0.16f, 0.98f, 1f);
+    public UnityEngine.Rendering.Universal.Light2D portalLight;
     float change = 1;
-    
+
+
     private void Start()
     {
+        portalLight = GetComponent<UnityEngine.Rendering.Universal.Light2D>();
+        portalLight.color = Color.Lerp(activeLightColor, inactiveLightColor, change);
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
         gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
     }
@@ -23,10 +31,12 @@ public class PortalController : MonoBehaviour
     }
 
     private void activate(){
+
         StartCoroutine(activateCoroutine());
     }
     public void deactivate(){
-
+        change = 1;
+        portalLight.color = Color.Lerp(activeLightColor, inactiveLightColor, change);
         m_SpriteRenderer.material.SetFloat("_TwirlSpeed", 0.2f);
         m_SpriteRenderer.material.SetFloat("_ColorTransition", 1);
     }
@@ -37,6 +47,7 @@ public class PortalController : MonoBehaviour
         m_SpriteRenderer.material.SetFloat("_TwirlSpeed", 0.6f);
         while (change > 0f)
         {
+            portalLight.color = Color.Lerp(activeLightColor, inactiveLightColor , change);
             m_SpriteRenderer.material.SetFloat("_ColorTransition", change);
             change -= 2 * Time.deltaTime;
             yield return new WaitForEndOfFrame();
