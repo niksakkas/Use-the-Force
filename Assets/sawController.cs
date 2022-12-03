d0189f8f47cdad8df1d18b0169aa1b56d1e4f8f8
nikitasakkas <nikitasakkas@gmail.com>
2022-12-03 20:45:41 +0200

    added saws, level 3 second block

---
+++ Assets/sawController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sawController : MonoBehaviour
+{
+    private Vector3 rotationIncrement = new Vector3(0, 0, 2f);
+
+    private void FixedUpdate()
+    {
+        //transform.Rotate(rotationIncrement);
+    }
+
+
+}
