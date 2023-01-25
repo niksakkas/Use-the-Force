e1715911314b54fec1dccebbd85589cc66393d6f
nikitasakkas <nikitasakkas@gmail.com>
2023-01-25 21:43:33 +0200

    death counter updates with a crossfade

--- Assets/Scripts/DeathCounter.cs
+++ Assets/Scripts/DeathCounter.cs
@@ -5,18 +5,39 @@ using TMPro;
 
 public class DeathCounter : MonoBehaviour
 {
-    public int deaths = 0;
+    public int deaths = 9;
     public TMP_Text deathCounterText;
+    public TMP_Text tempDeathCounterText;
+    private bool toogle = true;
 
     private void Start()
     {
-        deathCounterText.text = "Deaths: 0";
-    
+        tempDeathCounterText.alignment = TextAlignmentOptions.Left;
+        deathCounterText.alignment = TextAlignmentOptions.Left;
+        deathCounterText.text = "Deaths: 9";
+        tempDeathCounterText.text = "Deaths: 9";
+        //GetComponentsInChildren<TextMeshPro>().alignment = TextAlignmentOptions.Left;
+
         DontDestroyOnLoad(gameObject);
     }
     public void addDeath()
     {
         deaths++;
-        deathCounterText.text = "Deaths: " + deaths;
+
+
+        if (toogle == true)
+        {
+            tempDeathCounterText.text = "Deaths: " + deaths;
+            deathCounterText.CrossFadeAlpha(0f, 1f, false);
+            tempDeathCounterText.CrossFadeAlpha(1f, 1f, false);
+            toogle = false;
+        }
+        else
+        {
+            deathCounterText.text = "Deaths: " + deaths;
+            deathCounterText.CrossFadeAlpha(1f, 1f, false);
+            tempDeathCounterText.CrossFadeAlpha(0f, 1f, false);
+            toogle = true;
+        }
     }
 }
