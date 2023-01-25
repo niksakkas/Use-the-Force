69fc8aa6044ada25c550a50c76f1ea4580c431cc
nikitasakkas <nikitasakkas@gmail.com>
2023-01-25 23:39:47 +0200

    death counter bug fixes, added crossfade between scenes

--- Assets/Scripts/Special Objects/NextLevelController.cs
+++ Assets/Scripts/Special Objects/NextLevelController.cs
@@ -1,16 +1,54 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NextLevelController : MonoBehaviour
 {
+    public Image fadeImage;
+    [SerializeField] private float fadeDuration = 1f;
+    private int fadeIterations = 100;
+    private float iterationDuration;
+    private void Awake()
+    {
+        fadeImage = GameObject.FindGameObjectWithTag("FadeImage").GetComponent<Image>();
+        iterationDuration = fadeDuration / fadeIterations;
+    }
+    private void Start()
+    {
+        if (fadeImage.color.a > 0.99f)
+        {
+            StartCoroutine(enterCurrentLevel());
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.tag == "Player")
         {
-            Debug.Log("advance to next level!");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            StartCoroutine(leaveCurrentLevel());
+        }
+    }
+    IEnumerator leaveCurrentLevel()
+    {
+        Debug.Log("leave current level!");
+        for (int i = 0; i < fadeIterations; i++)
+        {
+            var temp = fadeImage.color;
+            temp.a += iterationDuration;
+            fadeImage.color = temp;
+            yield return new WaitForSeconds(iterationDuration);
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    IEnumerator enterCurrentLevel()
+    {
+        Debug.Log("enter new level!");
+        for (int i = 0; i < fadeIterations; i++)
+        {
+            var tempColor = fadeImage.color;
+            tempColor.a -= iterationDuration;
+            fadeImage.color = tempColor;
+            yield return new WaitForSeconds(iterationDuration);
         }
     }
 }
